40e7033b000d048b2606aa95a7521679b4bc7ec7
Purna <satichandrala@gmail.com>
2022-07-26 04:58:14 +0200

    variable initialization and reassignment

--- day3.sh
+++ day3.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# This  script is to understand the variables concept in bash scripting
+
+# Assign a value to a variable
 WORD="script"
-echo "${WORD}ing is fun"
+echo "${WORD}ing is fun!"
+
+# Change the value stored in  WORD variable.
+WORD="read"
+echo "${WORD}ing is fun!"
